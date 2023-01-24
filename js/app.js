05eb9ed6cb0effd8852b8b2f6c1ff279bfdaf651
Joel Myrtil <myrtil.joel@gmail.com>
2023-01-24 15:10:01 -0800

    completed class02.

---
+++ js/app.js
@@ -0,0 +1,71 @@
+console.log('hi')
+
+let whou = prompt("What's you're name")
+console.log('Hello ' + whou)
+
+let myname = prompt("Is my name Joel?").toLowerCase()
+  ;
+
+    if (myname === 'yes' || myname === 'y') { 
+      alert('You are correct!!')
+      //console.log('You are correct!!')
+    } else if (myname === 'no' || myname ==='n') {
+      alert('You are WRONG!!')
+      //console.log('You are WONRG!!')
+    } else
+      alert('Yes or No kindly')
+      //console.log('Yes or No kindly')
+
+let car = prompt("Do I Like Cars?").toLowerCase()
+  ;
+
+    if (car === 'yes' || car === 'y') { 
+      alert('You are correct!!')
+      //console.log('You are correct!!')
+    } else if (car === 'no' || car ==='n') {
+      alert('You are WRONG!!')
+      //console.log('You are WONRG!!')
+    } else
+      alert('Yes or No kindly')
+      //console.log('Yes or No kindly')
+
+let muzik = prompt("Do I Like EDM?").toLowerCase()
+  ;
+
+    if (muzik === 'yes' || muzik === 'y') { 
+      alert("You're headbaning right!!")
+      //console.log("You're headbaning right!!")
+    } else if (muzik === 'no' || muzik ==='n') {
+      alert('You are WRONG!!')
+      //console.log('You are WONRG!!')
+    } else
+      alert('Yes or No kindly')
+      //console.log('Yes or No kindly')
+
+let locate = prompt('Am I from Seattle?').toLowerCase()
+  ;
+
+    if (locate === 'no' || locate === 'n') { 
+      alert('You are correct!!')
+      //console.log('You are correct!!')
+    } else if (locate === 'yes' || locate ==='y') {
+      alert('You are WONRG!!')
+      //console.log('You are WONRG!!')
+    } else
+      alert('Yes or No kindly')
+      //console.log('Yes or No kindly')
+
+let weether = prompt("Do I Like the cold?").toLowerCase()
+  ;
+
+    if (weether === 'no' || weether === 'n') { 
+      alert("Ahhh, ahah that's hot!!")
+      //console.log("Ahhh, ahah that's hot!!")
+    } else if (weether === 'yes' || weether ==='y') {
+      alert('You are WRONG!!')
+      //console.log('You are WONRG!!')
+    } else
+      alert('Yes or No kindly')
+      //console.log('Yes or No kindly')
+
+alert('Was nice talking to you ' + whou + ' please do enjoy more information about myself.')
