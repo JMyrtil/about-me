c477be90ad0746c4d9511bc61e3aef0ff0ed0a4c
Joel Myrtil <myrtil.joel@gmail.com>
2023-01-25 22:52:21 -0800

    class 3 lab.

--- js/app.js
+++ js/app.js
@@ -6,68 +6,125 @@ let whou = prompt("What's you're name")
 console.log('Hello ' + whou)
 
 let myname = prompt("Is my name Joel?").toLowerCase()
-  ;
+    ;
 
-    if (myname === 'yes' || myname === 'y') { 
-      alert('You are correct!!')
-      //console.log('You are correct!!')
-    } else if (myname === 'no' || myname ==='n') {
-      alert('You are WRONG!!')
-      //console.log('You are WONRG!!')
-    } else
-      alert('Yes or No kindly')
-      //console.log('Yes or No kindly')
+if (myname === 'yes' || myname === 'y') {
+    alert('You are correct!!')
+    //console.log('You are correct!!')
+} else if (myname === 'no' || myname === 'n') {
+    alert('You are WRONG!!')
+    //console.log('You are WONRG!!')
+} else
+    alert('Yes or No kindly')
+//console.log('Yes or No kindly')
 
 let car = prompt("Do I Like Cars?").toLowerCase()
-  ;
+    ;
 
-    if (car === 'yes' || car === 'y') { 
-      alert('You are correct!!')
-      //console.log('You are correct!!')
-    } else if (car === 'no' || car ==='n') {
-      alert('You are WRONG!!')
-      //console.log('You are WONRG!!')
-    } else
-      alert('Yes or No kindly')
-      //console.log('Yes or No kindly')
+if (car === 'yes' || car === 'y') {
+    alert('You are correct!!')
+    //console.log('You are correct!!')
+} else if (car === 'no' || car === 'n') {
+    alert('You are WRONG!!')
+    //console.log('You are WONRG!!')
+} else
+    alert('Yes or No kindly')
+//console.log('Yes or No kindly')
 
 let muzik = prompt("Do I Like EDM?").toLowerCase()
-  ;
+    ;
 
-    if (muzik === 'yes' || muzik === 'y') { 
-      alert("You're headbaning right!!")
-      //console.log("You're headbaning right!!")
-    } else if (muzik === 'no' || muzik ==='n') {
-      alert('You are WRONG!!')
-      //console.log('You are WONRG!!')
-    } else
-      alert('Yes or No kindly')
-      //console.log('Yes or No kindly')
+if (muzik === 'yes' || muzik === 'y') {
+    alert("You're headbaning right!!")
+    //console.log("You're headbaning right!!")
+} else if (muzik === 'no' || muzik === 'n') {
+    alert('You are WRONG!!')
+    //console.log('You are WONRG!!')
+} else
+    alert('Yes or No kindly')
+//console.log('Yes or No kindly')
 
 let locate = prompt('Am I from Seattle?').toLowerCase()
-  ;
+    ;
 
-    if (locate === 'no' || locate === 'n') { 
-      alert('You are correct!!')
-      //console.log('You are correct!!')
-    } else if (locate === 'yes' || locate ==='y') {
-      alert('You are WONRG!!')
-      //console.log('You are WONRG!!')
-    } else
-      alert('Yes or No kindly')
-      //console.log('Yes or No kindly')
+if (locate === 'no' || locate === 'n') {
+    alert('You are correct!!')
+    //console.log('You are correct!!')
+} else if (locate === 'yes' || locate === 'y') {
+    alert('You are WONRG!!')
+    //console.log('You are WONRG!!')
+} else
+    alert('Yes or No kindly')
+//console.log('Yes or No kindly')
 
 let weether = prompt("Do I Like the cold?").toLowerCase()
-  ;
+    ;
 
-    if (weether === 'no' || weether === 'n') { 
-      alert("Ahhh, ahah that's hot!!")
-      //console.log("Ahhh, ahah that's hot!!")
-    } else if (weether === 'yes' || weether ==='y') {
-      alert('You are WRONG!!')
-      //console.log('You are WONRG!!')
-    } else
-      alert('Yes or No kindly')
-      //console.log('Yes or No kindly')
+if (weether === 'no' || weether === 'n') {
+    alert("Ahhh, ahah that's hot!!")
+    //console.log("Ahhh, ahah that's hot!!")
+} else if (weether === 'yes' || weether === 'y') {
+    alert('You are WRONG!!')
+    //console.log('You are WONRG!!')
+} else
+    alert('Yes or No kindly')
+//console.log('Yes or No kindly')
 
-alert('Was nice talking to you ' + whou + ' please do enjoy more information about myself.')
+let whp;
+let fast = 320;
+let slow = 319;
+let attemptsleft = 4;
+let rite = false;
+
+while (attemptsleft) {
+    whp = prompt(`How much whp does my car make?`);
+
+    if (whp > fast) {
+        attemptsleft --
+            alert(`Lower you have ${attemptsleft} tries left.`)
+        rite = true
+
+        if (rite = true) {
+            console.log(`incorrect`)
+        }
+    }
+
+    if (whp < fast) {
+        console.log(`incorrect`)
+        attemptsleft--
+        alert(`Higher you have ${attemptsleft} tries left.`)
+    } else {
+        console.log(`Correct`)
+        alert(`Fast Guy Huh!`)
+        break;
+    }
+}
+
+
+let mods = ['turbo', 'exhaust', 'intake'];
+let usercorrect = false;
+let tries = 6;
+let score = 0;
+let useranswer;
+
+while (tries) {
+    useranswer = prompt(`What mods do I have on my car?`);
+
+    for (let i = 0; i < mods.length; i++) {
+        if (useranswer === mods[i]) {
+            console.log(`Correct`)
+            usercorrect = true
+        }
+    }
+    if (usercorrect === true) {
+        alert(`Right`);
+        score++;
+        alert(score)
+        break;
+    } else {
+        tries--
+        alert(`Incorrect ${tries} left`)
+    }
+}
+
+alert('Was nice talking to you ${whou} please do enjoy more information about myself.')
